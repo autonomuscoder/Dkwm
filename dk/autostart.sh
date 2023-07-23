a3462c36d018a714e307c7b987e1657f9e4e0400
autonomuscoder <mahiabirgit@tutanota.com>
2023-07-23 12:26:11 +0600

    initial commit

---
+++ dk/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+picom --config ~/.config/picom/picom.conf &
+nm-applet &
+volumeicon &
+cbatticonn &
+polybar &
+nitrogen --restore &
+/usr/lib/policykit-1-gnome/polkit-authentication-agent-1 &
+dunst &
+numlockx on &
+xclip &
